e5b5595aed1eec0ae0d3ab6656ec963f4f8bd988
Hector MArtinez <hector_azul95@hotmail.com>
2017-10-08 19:32:51 -0500

    Agrego avance

--- proyectos/1/MartinezHector/proye_1.py
+++ proyectos/1/MartinezHector/proye_1.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python
+
 import os
 import stat
 import errno
 
 import fuse
-from fuse import fuse
+from fuse import Fuse
 if not hasattr(fuse, '__version__'):
-    raise RuntimeError  
-        
+    raise RuntimeError, \
+        "python-fuse doesn't know of fuse.__version__, probably it's too old."
 fuse.fuse_python_api = (0, 2)
 
 import logging
@@ -90,7 +92,7 @@ class DictFS(Fuse):
         # Ask for root dir
         if path == '/':
             #return self.root.stats
-            st.st_mode = stat.S_IFDIR 
+            st.st_mode = stat.S_IFDIR | 0755
             st.st_nlink = 2
             return st
 
@@ -100,13 +102,13 @@ class DictFS(Fuse):
             # Entry found
             # is a directory?
             if type(level[entry]) is dict:
-                st.st_mode = stat.S_IFDIR 
+                st.st_mode = stat.S_IFDIR | 0755 # rwx r-x r-x
                 st.st_nlink = 2
                 logging.debug('*** getattr_dir_found: %s', entry)
                 return st
             # is a file?
             if type(level[entry]) is str:
-                st.st_mode = stat.S_IFREG #| 0666 # rw- rw- rw-
+                st.st_mode = stat.S_IFREG | 0666 # rw- rw- rw-
                 st.st_nlink = 1
                 st.st_size = len(level[entry])
                 logging.debug('*** getattr_file_found: %s', entry)
@@ -116,4 +118,165 @@ class DictFS(Fuse):
         logging.debug('*** getattr_entry_not_found')
         return -errno.ENOENT
 
-   
+    def readdir(self, path, offset):
+        logging.debug('*** readdir(%s, %d)', path, offset)       
+
+        file_entries = ['.','..']
+
+        # Get filelist
+        level = self.__get_dir(path)
+
+        # Get all directory entries
+        if len(level.keys()) > 0:
+            file_entries += level.keys()
+
+        file_entries = file_entries[offset:]
+        for filename in file_entries:
+            yield fuse.Direntry(filename)
+
+    def mkdir ( self, path, mode ):
+        logging.debug('*** mkdir(%s, %d)', path, mode)
+
+        level, entry = self.__navigate(path)
+
+        # Make new dir
+        level[entry] = {}
+
+    def mknod ( self, path, mode, dev ):
+        logging.debug('*** mknod(%s, %d, %d)', path, mode, dev)
+
+        level, filename = self.__navigate(path)
+
+        # Make empty file
+        level[filename] = ''
+
+    # This method could maintain opened (or locked) file list and,
+    # of course, it could check file permissions.
+    # For now, only check if file exists...
+    def open ( self, path, flags ):
+        logging.debug('*** open(%s, %d)', path, flags)
+
+        level, filename = self.__navigate(path)
+
+        # File exists?
+        if not level.has_key(filename):
+            return -errno.ENOENT
+
+        # No exception or no error means OK
+
+    # In this example this method is the same as open(). This method
+    # is called by close() syscall, it's means that if open() maintain
+    # an opened-file list, or lock files, or something... this method
+    # must do reverse operation (refresh opened-file list, unlock files...
+    def release ( self, path, flags ):
+        logging.debug('*** release(%s, %d)', path, flags)
+
+        level, filename = self.__navigate(path)
+
+        # File exists?
+        if not level.has_key(filename):
+            return -errno.ENOENT
+
+    def read ( self, path, length, offset ):
+        logging.debug('*** read(%s, %d, %d)', path, length, offset)
+
+        level, filename = self.__navigate(path)
+
+        # File exists?
+        if not level.has_key(filename):
+            return -errno.ENOENT
+
+        # Check ranges
+        file_size = len(level[filename])
+        if offset < file_size:
+            # Fix size
+            if offset + length > file_size:
+                length = file_size - offset
+            buf = level[filename][offset:offset + length]
+        else:
+            # Invalid range returns no data, instead error!
+            buf = ''
+        return buf
+
+    def rmdir ( self, path ):
+        logging.debug('*** rmdir(%s)', path)
+
+        level, entry = self.__navigate(path)
+
+        # File exists?
+        if not level.has_key(entry):
+            return -errno.ENOENT
+
+        # Delete entry
+        del(level[entry])
+
+    def truncate ( self, path, size ): ###Error
+        logging.debug('*** truncate(%s, %d)', path, size)
+
+        level, filename = self.__navigate(path)
+
+        # File exists?
+        if not level.has_key(filename):
+            return -errno.ENOENT
+
+        if len(level[filename]) > size:
+            # Truncate file to specified size
+            level[filename] = level[filename][:size]
+        else:
+            # Add more bytes
+            level[filename] += ' ' * (size - len(level[filename]))
+        
+    def unlink ( self, path ):
+        logging.debug('*** unlink(%s)', path)
+
+        level, entry = self.__navigate(path)
+
+        # File exists?
+        if not level.has_key(entry):
+            return -errno.ENOENT
+
+        # Remove entry
+        del(level[entry])
+
+    def write ( self, path, buf, offset ):
+        logging.debug('*** write(%s, %s, %d)', path, str(buf), offset)
+
+        level, filename = self.__navigate(path)
+
+        # Write data into file
+        if offset > len(level[filename]):
+            offset = (offset % len(level[filename]))
+        level[filename] = level[filename][:offset] + str(buf)
+        
+        # Return written bytes
+        return len(buf)
+
+    def rename ( self, oldPath, newPath ):
+        logging.debug('*** rename(%s, %s)', oldPath, newPath)
+
+        oldLevel, oldFilename = self.__navigate(oldPath)
+        # Can't use __navigate() because newPath-filename not exists
+        newPath = self.__path_list(newPath)
+        newFilename = newPath.pop()
+        newLevel = self.__get_dir(self.__join_path(newPath))
+
+        # Make new link
+        newLevel[newFilename] = oldLevel[oldFilename]
+
+        # Remove old
+        self.unlink(oldPath)
+
+def main():
+    usage = """
+Userspace filesystem example
+""" + Fuse.fusage
+    
+    fs = DictFS(version = '%prog' + fuse.__version__,
+                usage = usage,
+                dash_s_do='setsingle')
+    fs.parse(errex = 1)
+    fs.main()
+
+if __name__ == '__main__':
+    main()
+
