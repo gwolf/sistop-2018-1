ff35e3807eae650a40af182ea445400aefecbdd9
Hector MArtinez <hector_azul95@hotmail.com>
2017-11-18 23:26:01 -0600

    Agrego funciones pensadas a usar

---
+++ proyectos/2/MartinezHector-FuentesJaziel/Monitor.py
@@ -0,0 +1,25 @@
+#Biblotecas
+import curses
+import psutil
+import threading
+import time
+
+
+#Variables
+
+#Informacion de ultimos procesos
+def procs():
+
+
+#inicalizacion de la barrera
+def sync():
+
+
+#f porcentaje
+def porcentaje():
+
+#memoria usada y disponible
+def mem_usage():
+
+#interfaz
+def Interfaz():
