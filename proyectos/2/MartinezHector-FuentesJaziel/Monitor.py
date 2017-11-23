4ca4af396801f78f1907d8be91387613cf7ca1ac
Hector MArtinez <hector_azul95@hotmail.com>
2017-11-23 01:01:02 -0600

    Agrego avance

--- proyectos/2/MartinezHector-FuentesJaziel/Monitor.py
+++ proyectos/2/MartinezHector-FuentesJaziel/Monitor.py
@@ -6,19 +6,36 @@ import time
 
 
 #Variables
+global num_hlos
+global conta
+global mutex
+global barrera
+num_hlos = 4
+conta = 0
+mutex = threading.Semaphore(1)
+barrera= threading.Semaphore(0)
 
 #Informacion de ultimos procesos
 def procs():
+	lstproc = psutil.pids()
+	lstproc.reverse()
+
+
+	for x in xrange(0,size):
+		myscreen.addstr(10+x, 2,"%d "%lstproc[x])
+		myscreen.addstr(10+x, 8,psutil.Process(lstproc[x]).name()+"         ")
+		myscreen.addstr(10+x, 22,psutil.Process(lstproc[x]).status()+"  ")
+		myscreen.addstr(10+x, 35,"%d   "%psutil.Process(lstproc[x]).num_threads())
 
 
 #inicalizacion de la barrera
 def sync():
-	global contador
+	global conta
 	mutex.acquire()
-	contador=contador+1
+	conta=conta+1
 	mutex.release()
 
-	if contador == num_hlos:
+	if conta == num_hlos:
 		barrera.release()
 	barrera.acquire()
 	barrera.release()
@@ -29,11 +46,7 @@ def sync():
 def porcentaje():
 
 #memoria usada y disponible
-def mem_usage():
-	myscreen.addstr(7,45,sizeof_fmt(psutil.virtual_memory().available))
-	myscreen.addstr(7,71,sizeof_fmt(psutil.virtual_memory().used))
-	
-	sync()
+
 
 
 #interfaz
