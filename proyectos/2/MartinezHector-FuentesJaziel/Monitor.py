342d9221f72865f762337070510c3ac41ed1d21d
Hector MArtinez <hector_azul95@hotmail.com>
2017-11-23 00:52:14 -0600

    Creacion de unas funciones

--- proyectos/2/MartinezHector-FuentesJaziel/Monitor.py
+++ proyectos/2/MartinezHector-FuentesJaziel/Monitor.py
@@ -13,6 +13,16 @@ def procs():
 
 #inicalizacion de la barrera
 def sync():
+	global contador
+	mutex.acquire()
+	contador=contador+1
+	mutex.release()
+
+	if contador == num_hlos:
+		barrera.release()
+	barrera.acquire()
+	barrera.release()
+
 
 
 #f porcentaje
@@ -20,6 +30,11 @@ def porcentaje():
 
 #memoria usada y disponible
 def mem_usage():
+	myscreen.addstr(7,45,sizeof_fmt(psutil.virtual_memory().available))
+	myscreen.addstr(7,71,sizeof_fmt(psutil.virtual_memory().used))
+	
+	sync()
+
 
 #interfaz
 def Interfaz():
